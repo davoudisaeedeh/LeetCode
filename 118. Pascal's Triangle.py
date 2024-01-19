2e383a6c1d1230914ad8eebc04091f1c713dc793
saeedehdavoudi <d.saeedeh74@gmail.com>
2024-01-18 21:46:16 -0700

    modified:   118. Pascal's Triangle.py

--- 118. Pascal's Triangle.py
+++ 118. Pascal's Triangle.py
@@ -1,15 +1,10 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        if numRows == 1:
-            return [[1]]
-        elif numRows == 2:
-            return [[1], [1,1]]
-        else:
-            list_of_lists = [[1], [1,1]]
-            for _ in range(numRows-2):
-                new = [1]
-                for i in range(1, len(list_of_lists[-1])):
-                    new.append(list_of_lists[-1][i]+list_of_lists[-1][i-1])
-                new.append(1)
-                list_of_lists.append(new)
-            return list_of_lists
+        lists = []
+        for i in range(1, numRows+1):
+            new = [1] * i
+            for j in range(1, (i-1)//2+1):
+                val = lists[-1][j]+lists[-1][j-1]
+                new[j], new[-j-1] = val, val
+            lists.append(new)
+        return lists
